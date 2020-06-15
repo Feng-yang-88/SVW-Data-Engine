2a9882450a88604d26c5912cce04a2fc378d6563
Feng-yang-88 <66940035+Feng-yang-88@users.noreply.github.com>
2020-06-15 16:02:22 +0800

    Add files via upload

---
+++ 作业1 求和.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# 2+4+6+........+100的和
+sum = 0
+number = 1
+while number < 51:
+    sum = sum + number * 2
+    number = number + 1
+print(sum)
+
+
+# In[ ]:
+
+
+
+
